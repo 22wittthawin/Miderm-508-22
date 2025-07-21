c7b03d771197d3df05ee74aff6ba0b38a402aa21
22wittthawin <witthawinhomon@gmail.com>
2025-07-21 14:22:59 +0700

    Update Midterm_4.py

--- Midterm_4.py
+++ Midterm_4.py
@@ -1,11 +1,25 @@
-numbers = []
+i = 0
+while i <= 100:
+    print(i)
+    i += 1
 
-for i in range(5):
-    num = int(input())
-    numbers.append(num)
+i = 1000
+while i >= 0:
+    print(i)
+    i -= 1
 
-total = sum(numbers)
-average = total / 5
+min_num = None 
+while True:
+    try:
+        num = float(input("กรอกจำนวนจริงบวก: "))
+        if num <= 0:
+            break
+        if min_num is None or num < min_num:
+            min_num = num
+    except:
+        break
 
-print(total)
-print(average)
+if min_num is not None:
+    print("จำนวนจริงบวกที่น้อยที่สุดคือ:", min_num)
+else:
+    print("ไม่มีข้อมูลจำนวนจริงบวก")
